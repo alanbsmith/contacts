f5bd170a3b3ad30d275286671d50af0039002127
Alan Smith <a.bax.smith@gmail.com>
2016-07-15 09:11:51 -0600

    refactor functionality

--- index.js
+++ index.js
@@ -19,51 +19,74 @@ let contactList = [
 ];
 
 $(document).ready(() => {
-  clearSearchInput();
-  buildContactList(contactList);
+  const contacts = new Contacts(contactList);
+  contacts.buildList(contactList);
+
+  $('input#search').keyup((e) => {
+    const results = contacts.search(e.target.value);
+    contacts.buildList(results);
+  });
+
+  $('.sort-link').click((e) => {
+    let sorted;
+    if(e.target.id === "last-name") {
+      sorted = contacts.sortByLastName();
+    } else {
+      sorted = contacts.sortByFirstName();
+    }
+    return contacts.buildList(sorted);
+
+  });
 });
 
-const buildContactList = (contacts) => {
-  return contacts.map((contact, i) => {
-    $('.contact-list').append(
-      '<div class="contact">' +
-        '<p class="name" id=`${i}`>' +
-          `${contact}` +
-        '</p>' +
-      '</div>'
-    )
-  });
+class Contacts {
+  constructor(initialContacts) {
+    // list of all contacts
+    this.list = initialContacts;
+    // current list of contacts displayed in the view
+    this.currentList = [];
+  };
+
+  // setting the current list of contacts to be displayed
+  setCurrentList(list) {
+    currentList = list;
+  };
+
+  // build / rebuild the list in the view
+  buildList(contacts) {
+    // update the current list of contacts
+    this.currentList = contacts;
+    // reset the list before appending
+    $('.contact').remove();
+    return contacts.map((contact, i) => {
+      $('.contact-list').append(
+        '<div class="contact">' +
+          '<p class="name" id=`${i}`>' +
+            `${contact}` +
+          '</p>' +
+        '</div>'
+      )
+    });
+  };
+
+  search(query) {
+    const results = this.list.filter((contact) => {
+      return contact.toLowerCase().includes(query)
+    });
+    return results;
+
+  };
+
+  sortByFirstName() {
+    return this.currentList.sort();
+  };
+
+  sortByLastName() {
+    const sorted = this.currentList.sort((x,y) => {
+      const current = x.split(' ').pop();
+      const next = y.split(' ').pop();
+      return current < next ? -1 : current > next ? 1 : 0;
+    });
+    return sorted;
+  };
 };
-
-const sortByLastName = () => {
-  const sorted = contactList.sort((x,y) => {
-    first = x.split(' ').pop();
-    next = y.split(' ').pop();
-    return first < next ? -1 : first > next ? 1 : 0;
-  });
-  return buildContactList(sorted);
-};
-
-$('.sort-link').click((e) => {
-  clearContactList()
-  if(e.target.id === "last-name") {
-    return sortByLastName();
-  }
-  return buildContactList(contactList.sort());
-});
-
-$('input#search').keyup((e) => {
-  const results = contactList.filter((contact) => {
-    return contact.toLowerCase().includes(e.target.value)
-  });
-  clearContactList();
-  return buildContactList(results);
-});
-
-const clearContactList = () => {
-  $('.contact').remove();
-};
-
-const clearSearchInput = () => {
-  $('input#search').value = '';
-}
