540d1a5b54d0e7f14242481f55116e81693bd923
Chad Ellison <chad.ellison0123@gmail.com>
2016-08-11 22:26:43 -0600

    Make contacts dynamic

--- index.js
+++ index.js
@@ -9,3 +9,36 @@ const hello = (name) => {
 
 oldHello('world');
 hello('es6');
+
+
+$(document).ready(function(){
+  listContacts()
+})
+
+var contacts = [
+  "Andy Mention",
+  "Emily Davis",
+  "Eric Fransen",
+  "Jessica Goulding",
+  "Jonmichael Chambers",
+  "Marc Garreau",
+  "Tan Doan",
+  "Alan Smith",
+  "Allison Larson",
+  "Andrw Watkins",
+  "Chad Brading",
+  "Corey Davis",
+  "Gustavo Villagrana",
+  "Hilary Denton",
+  "Horacio Chavez",
+  "Tim Proctor",
+  "Will Faurot"
+]
+
+function listContacts() {
+  contacts.forEach(function(contact) {
+    $(".contact-list").append(
+      "<div class='contact'><p class='name'> " + contact + "</p></div>"
+    )
+  })
+}
